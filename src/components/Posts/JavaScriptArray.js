4d6d4c3ff54f2fd9173f53fe064552864881e430
Adam Harvey <heyitsmeharv@gmail.com>
2023-04-12 21:50:31 +0100

    corrections

--- src/components/Posts/JavaScriptArray.js
+++ src/components/Posts/JavaScriptArray.js
@@ -68,21 +68,32 @@ const JavaScriptArray = () => {
         <Title>JavaScript Arrays</Title>
         <Spacer />
         <Text>
-          Do you ever find yourself forgetting what function would be best to manipulate your data?
-          In this post, I would like to go through different use cases for manipulating your data.
-          First let's tackle what an array is exactly...
+          Do you ever find yourself forgetting what function would be best to manipulate your data? In this post, I would like to go through different use cases for manipulating your data.
           <Spacer />
-          <BoldText>Arrays are technically objects which help with storing a collection of multiple data sets under a single variable name. They also come with handy operations which help with manipulating your data. </BoldText>
+          But before we get into that, I'd just like to briefly tackle what an array is exactly and some useful tips. If you're not new to JavaScript, you can probably skip this part.
+          <Spacer />
+          <BoldText>"Arrays are technically objects which help with storing a collection of multiple data sets under a single variable name. They also come with handy operations which help with manipulating your data."</BoldText>
           <Spacer />
           Things to note:
           <li>JavaScript arrays are resizable and can contain a mix of different data types (integers, object and even arrays themselves)</li>
-          <li>JavaScript arrays are zero-indexed, which means they start at index 0, the second index being 1 and so forth. If you're wanting to get the last element of the array - you can always get the length of the array minus 1</li>
+          <CodeBlock>
+            const starWarsCharacters = [ "starwars", {`starwars: [ { nameOne: "Luke", nameTwo: "Leia", nameThree: "Han" } ]`}, 4, ["starwars"] ];
+          </CodeBlock>
+          <li>JavaScript arrays are zero-indexed, which means they start at index 0, the second index being 1 and so forth.</li>
           <CodeBlock>
             const starWarsCharacters = [];
             <Spacer />
             starWarsCharacters.push("Luke", "Leia", "Han");
             <Spacer />
-            console.log(starWarsCharacters.length); // 3
+            console.log(starWarsCharacters[0]); // "Luke"
+            <Spacer />
+          </CodeBlock>
+          <li>If you're wanting to get the last element of an array - you can always get the length of the array minus 1</li>
+          <CodeBlock>
+            const starWarsCharacters = ["Luke", "Leia", "Han"];
+            <Spacer />
+            console.log(starWarsCharacters[starWarsCharacters.length - 1]); // "Han"
+            <Spacer />
           </CodeBlock>
         </Text>
       </Container>
