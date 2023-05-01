a1371a075bce0322b82c66bf4b71b72361a4c8e6
Adam Harvey <heyitsmeharv@gmail.com>
2023-05-01 18:29:07 +0100

    fixed dropdown logic

--- src/components/ArrayExplorer/ArrayExplorer.js
+++ src/components/ArrayExplorer/ArrayExplorer.js
@@ -8,62 +8,68 @@ const Text = styled.span`
   font-size: 2rem;
 `;
 
+const Spacer = styled.br``;
+
 const ArrayExplorer = () => {
   const [selectedOptions, setSelectedOptions] = useState([]);
-  // const [secondSelectedOptions, setSecondSelectedOptions] = useState([]);
+  const [secondSelectedOptions, setSecondSelectedOptions] = useState([]);
+  const [thirdSelectedOptions, setThirdSelectedOptions] = useState([]);
 
   const options = [
-    { value: 'I need to add', label: 'Add items or other arrays', secondOptions: [{ value: 'TEST', label: 'Elements/s to an array' }, { value: 'TEST', label: 'Elements/s to the end of an array' }, { value: 'TEST', label: 'Elements/s to the front of an array' }, { value: 'TEST', label: 'this array to other array(s) and/or value(s)' }] },
-    { value: 'I need to remove', label: 'Remove items', secondOptions: [{ value: 'TEST', label: 'Elements/s from an array' }, { value: 'TEST', label: 'The last element of the array' }, { value: 'TEST', label: 'The first element of an array' }, { value: 'TEST', label: 'one or more elements in order for use, leaving the array as is' }] },
-    { value: `I'm trying to find`, label: 'Find items', secondOptions: [{ value: 'TEST', label: 'one item' }, { value: 'TEST', label: 'one or many items' }], thirdOptions: [{ value: 'TEST', label: 'Values based on a condition I create' }, { value: 'TEST', label: 'Whether or not every item satisfies a condition' }, { value: 'TEST', label: 'Whether or not at least one item satisfies a condition' }] },
+    { value: 'I need to add', label: 'Add items or other arrays', secondOptions: [{ value: 'I need to add', label: 'Elements/s to an array' }, { value: 'I need to add', label: 'Elements/s to the end of an array' }, { value: 'I need to add', label: 'Elements/s to the front of an array' }, { value: 'I need to add', label: 'this array to other array(s) and/or value(s)' }] },
+    { value: 'I need to remove', label: 'Remove items', secondOptions: [{ value: 'I need to remove', label: 'Elements/s from an array' }, { value: 'I need to remove', label: 'The last element of the array' }, { value: 'I need to remove', label: 'The first element of an array' }, { value: 'I need to remove', label: 'one or more elements in order for use, leaving the array as is' }] },
+    { value: `I'm trying to find`, label: 'Find items', secondOptions: [{ value: `I'm trying to find`, label: 'one item' }, { value: `I'm trying to find`, label: 'one or many items' }], thirdOptions: [{ value: 'I need to find', label: 'Values based on a condition I create' }, { value: 'TEST', label: 'Whether or not every item satisfies a condition' }, { value: 'I need to find', label: 'Whether or not at least one item satisfies a condition' }] },
     { value: 'I need to iterate by', label: 'Walk over items', secondOptions: [] },
     { value: 'I need to', label: 'Return a string', secondOptions: [] },
     { value: 'I need to', label: 'Order an array', secondOptions: [] },
     { value: 'I need to', label: 'Something else', secondOptions: [] },
   ];
 
-  // const secondSelectOptions = [
-  //   { value: 'add', label: 'Add items or other arrays' },
-  //   { value: 'remove', label: 'Remove items' },
-  //   { value: 'find', label: 'Find items ' },
-  //   { value: 'walk', label: 'Walk over items' },
-  //   { value: 'return', label: 'Return a string' },
-  //   { value: 'order', label: 'Order an array' },
-  //   { value: 'other', label: 'Something else' },
-  // ];
-
-  const handleSelectChange = (order, s) => {
-    // return order === 'first' ? setFirstSelectedOptions(s) : setSecondSelectedOptions(s);
-    setSelectedOptions(s)
+  const handleSelectChange = (selected, option) => {
+    switch (option) {
+      case "first":
+        setSelectedOptions(selected)
+        break;
+      case "second":
+        setSecondSelectedOptions(selected)
+        break;
+      case "third":
+        setThirdSelectedOptions(selected)
+        break;
+      default:
+    }
   };
+
+  console.log('selectedOptions', selectedOptions);
+  console.log('secondSelectedOptions', secondSelectedOptions);
 
   return (
     <>
       <Select
         options={options}
         value={selectedOptions}
-        onChange={s => handleSelectChange('first', s)}
+        onChange={s => handleSelectChange(s, 'first')}
       />
       {Object.keys(selectedOptions).length !== 0 ?
         <>
-          <Text>{selectedOptions?.value}</Text>
-          <Select
-            options={selectedOptions?.secondOptions}
-            value={selectedOptions}
-            onChange={s => handleSelectChange('second', s)}
-          />
-        </>
-        : ''}
-      {/* {Object.keys(selectedOptions.thirdOptions).length !== 0 ?
-        <>
           <Text>{selectedOptions.value}</Text>
           <Select
-            options={selectedOptions.thirdOptions}
-            value={selectedOptions}
-            onChange={s => handleSelectChange('third', s)}
+            options={selectedOptions.secondOptions}
+            value={secondSelectedOptions}
+            onChange={s => handleSelectChange(s, 'second')}
           />
+          <Spacer />
+          {secondSelectedOptions && Object.keys(secondSelectedOptions).length !== 0 && selectedOptions.thirdOptions ?
+            <>
+              <Select
+                options={selectedOptions.thirdOptions}
+                value={thirdSelectedOptions}
+                onChange={s => handleSelectChange(s, 'third')}
+              />
+            </>
+            : ''}
         </>
-        : ''} */}
+        : ''}
     </>
   );
 }
