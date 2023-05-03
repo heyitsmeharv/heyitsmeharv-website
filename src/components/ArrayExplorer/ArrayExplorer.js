08270d43d7a632a25df200c84c93a68d5e0e2c91
Adam Harvey <heyitsmeharv@gmail.com>
2023-05-03 11:34:06 +0100

    styling of dropdown and usage and output added

--- src/components/ArrayExplorer/ArrayExplorer.js
+++ src/components/ArrayExplorer/ArrayExplorer.js
@@ -1,11 +1,47 @@
 import React, { useState } from "react";
 import styled from "styled-components";
 
+// animations
+import SlideInBottom from "../../animations/SlideInBottom";
+
+// components
 import Select from 'react-select';
+
+// examples
+import { addExampleUsage, addExampleOutput } from "./codeExamples";
+
+const Container = styled.div`
+  animation: ${SlideInBottom} 0.5s forwards;
+`;
+
+const StyledSelect = styled(Select)`
+  & .Select__menu {
+    color: ${({ theme }) => theme.text};
+    background:  ${({ theme }) => theme.secondary};
+  }
+
+  & .Select__option {
+    &:hover {
+      color: ${({ theme }) => theme.primary};
+    }
+  }
+`;
 
 const Text = styled.span`
   color: ${({ theme }) => theme.text};
   font-size: 2rem;
+`;
+
+const CodeBlock = styled.pre`
+  font-family: 'monospace';
+  font-size: 2rem;
+  background: #292929;
+  color: ${({ theme }) => theme.buttonText};
+  word-wrap: break-word;
+  padding: 1rem 2rem 1rem;
+  border-radius: 2rem;
+  overflow-x: auto;
+  line-height: 3.5rem;
 `;
 
 const Spacer = styled.br``;
@@ -16,7 +52,7 @@ const ArrayExplorer = () => {
   const [thirdSelectedOptions, setThirdSelectedOptions] = useState([]);
 
   const options = [
-    { value: 'I need to add', label: 'Add items or other arrays', secondOptions: [{ value: 'I need to add', label: 'Elements/s to an array' }, { value: 'I need to add', label: 'Elements/s to the end of an array' }, { value: 'I need to add', label: 'Elements/s to the front of an array' }, { value: 'I need to add', label: 'this array to other array(s) and/or value(s)' }] },
+    { value: 'I need to add', label: 'Add items or other arrays', secondOptions: [{ value: 'I need to add', label: 'Elements/s to an array', usage: addExampleUsage, output: addExampleOutput }, { value: 'I need to add', label: 'Elements/s to the end of an array' }, { value: 'I need to add', label: 'Elements/s to the front of an array' }, { value: 'I need to add', label: 'this array to other array(s) and/or value(s)' }] },
     { value: 'I need to remove', label: 'Remove items', secondOptions: [{ value: 'I need to remove', label: 'Elements/s from an array' }, { value: 'I need to remove', label: 'The last element of the array' }, { value: 'I need to remove', label: 'The first element of an array' }, { value: 'I need to remove', label: 'one or more elements in order for use, leaving the array as is' }] },
     { value: `I'm trying to find`, label: 'Find items', secondOptions: [{ value: `I'm trying to find`, label: 'one item' }, { value: `I'm trying to find`, label: 'one or many items' }], thirdOptions: [{ value: 'I need to find', label: 'Values based on a condition I create' }, { value: 'TEST', label: 'Whether or not every item satisfies a condition' }, { value: 'I need to find', label: 'Whether or not at least one item satisfies a condition' }] },
     { value: 'I need to iterate by', label: 'Walk over items', secondOptions: [] },
@@ -45,7 +81,8 @@ const ArrayExplorer = () => {
 
   return (
     <>
-      <Select
+      <StyledSelect
+        classNamePrefix={'Select'}
         options={options}
         value={selectedOptions}
         onChange={s => handleSelectChange(s, 'first')}
@@ -53,7 +90,8 @@ const ArrayExplorer = () => {
       {Object.keys(selectedOptions).length !== 0 ?
         <>
           <Text>{selectedOptions.value}</Text>
-          <Select
+          <StyledSelect
+            classNamePrefix={'Select'}
             options={selectedOptions.secondOptions}
             value={secondSelectedOptions}
             onChange={s => handleSelectChange(s, 'second')}
@@ -61,7 +99,8 @@ const ArrayExplorer = () => {
           <Spacer />
           {secondSelectedOptions && Object.keys(secondSelectedOptions).length !== 0 && selectedOptions.thirdOptions ?
             <>
-              <Select
+              <StyledSelect
+                classNamePrefix={'Select'}
                 options={selectedOptions.thirdOptions}
                 value={thirdSelectedOptions}
                 onChange={s => handleSelectChange(s, 'third')}
@@ -70,6 +109,20 @@ const ArrayExplorer = () => {
             : ''}
         </>
         : ''}
+      <Container>
+        {secondSelectedOptions.usage &&
+          <>
+            <Text>Usage</Text>
+            <CodeBlock>{secondSelectedOptions.usage}</CodeBlock>
+          </>
+        }
+        {secondSelectedOptions.output &&
+          <>
+            <Text>Output</Text>
+            <CodeBlock>{secondSelectedOptions.output}</CodeBlock>
+          </>
+        }
+      </Container>
     </>
   );
 }
---
+++ src/components/ArrayExplorer/codeExamples.js
@@ -0,0 +1,5 @@
+export const addExampleUsage = `let arr = [5, 1, 8];
+arr.splice(2, 0, 'tacos');
+console.log(arr);`;
+
+export const addExampleOutput = `[5, 1, 'tacos', 8]`;
