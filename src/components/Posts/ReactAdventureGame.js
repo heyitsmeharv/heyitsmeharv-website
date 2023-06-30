53409594b1da5d558881381099c2887be6608a8c
Adam Harvey <heyitsmeharv@gmail.com>
2023-06-30 17:45:44 +0100

    fleshed out post.

--- src/components/Posts/ReactAdventureGame.js
+++ src/components/Posts/ReactAdventureGame.js
@@ -13,7 +13,7 @@ import { JavascriptSVG, ReactjsSVG } from '../../resources/styles/icons';
 import { StyledNavButton, StyledNavLink, CopyButton } from '../Button/Button';
 
 // codeblocks
-import { createReactAppCommand, mapGeneration } from "../../helpers/codeblocks";
+import { createReactAppCommand, mapGeneration, roomHelper } from "../../helpers/codeblocks";
 
 const Wrapper = styled.div`
   padding: 1rem 25%;
@@ -69,6 +69,11 @@ const SubTitle = styled.h1`
   font-style: italic;
 `;
 
+const SubTitleSmall = styled.h1`
+  font-size: 2rem;
+  font-weight: bold;
+`;
+
 const Text = styled.span`
   color: ${({ theme }) => theme.text};
   font-size: 2rem;
@@ -78,6 +83,13 @@ const BoldText = styled.b`
   color: ${({ theme }) => theme.text};
   font-size: 2rem;
   font-weight: bold;
+`;
+
+const StyledCodeSpan = styled.code`
+  background-color: #f1f1f1;
+  color: crimson;
+  padding: 0 5px;
+  margin: 0 5px;
 `;
 
 const StyledListItem = styled.li`
@@ -110,7 +122,10 @@ const Spacer = styled.br``
 const ReactAdventureGame = () => {
   const [isCopied, setIsCopied] = useState([
     { value: false },
-    { value: false }
+    { value: false },
+    { value: false },
+    { value: false },
+    { value: false },
   ]);
 
   // analytics
@@ -121,7 +136,10 @@ const ReactAdventureGame = () => {
   const handleCopy = (code, key) => {
     const isCopiedDefault = [
       { value: false },
-      { value: false }
+      { value: false },
+      { value: false },
+      { value: false },
+      { value: false },
     ];
     navigator.clipboard.writeText(code);
 
@@ -153,23 +171,69 @@ const ReactAdventureGame = () => {
           I've been contemplating on whether or not to do a separate blog post to explain what the React.js framework is but I want to move away from my last posts and get stuck in with building something!
           If you're completely new to React I would recommend having a gander at their  <StyledAnchor href="https://react.dev/" target="_blank">documentation</StyledAnchor> which is pretty good.
           <Spacer />
-          I'll break this down into sections:
-          <StyledAnchor href="#project-setup"><StyledListItem>Project Setup</StyledListItem></StyledAnchor>
-          <StyledAnchor href="#map-generation"><StyledListItem>Map Generation</StyledListItem></StyledAnchor>
+          If you would rather just go look at the code for yourself then feel free. You can find it here - <StyledAnchor href="https://github.com/heyitsmeharv/react-text-based-adventure" target="_blank">source</StyledAnchor>
           <Spacer />
-          <SubTitle id='project-setup'>Project Setup</SubTitle>
-          <CopyButton onClick={() => handleCopy(createReactAppCommand, 0)}>
-            {isCopied[0].value === true ? 'Copied!' : 'Copy'}
-          </CopyButton>
-          <CodeBlock>{createReactAppCommand}</CodeBlock>
+          I don't plan on going through this line by line but what I will do is cover the core features of this project which I'll break down into sections:
+          <StyledAnchor href="#map-generation"><StyledListItem>Map Generation</StyledListItem></StyledAnchor>
+          <StyledAnchor href="#character"><StyledListItem>Character</StyledListItem></StyledAnchor>
+          <StyledAnchor href="#loot"><StyledListItem>Loot</StyledListItem></StyledAnchor>
+          <StyledAnchor href="#combat"><StyledListItem>Combat</StyledListItem></StyledAnchor>
+          <StyledAnchor href="#findings"><StyledListItem>Findings</StyledListItem></StyledAnchor>
           <Spacer />
           <SubTitle id='map-generation'>Map Generation</SubTitle>
           So, let's talk map generation. I had a couple of choices, I could have gone for a file import option, where I could parse a text file which letters and symbols to represent certain features such as items and enemies etc.
           Seeing as thought this text based adventure is linear, I've opted to predefine the potential map with 'room objects' where I can randomise the order each run through.
-          <CopyButton onClick={() => handleCopy(mapGeneration, 1)}>
-            {isCopied[1].value === true ? 'Copied!' : 'Copy'}
+          <Spacer />
+          <SubTitleSmall>helpers/rooms.js</SubTitleSmall>
+          I've defined my list of rooms in a helper file, each room is an object which will store all the attributes required for the room such as
+          <StyledCodeSpan>name</StyledCodeSpan>
+          <StyledCodeSpan>description</StyledCodeSpan>
+          <StyledCodeSpan>items</StyledCodeSpan>
+          <StyledCodeSpan>enemies</StyledCodeSpan>
+          <StyledCodeSpan>locked</StyledCodeSpan>
+          <StyledCodeSpan>explored</StyledCodeSpan>.
+          <CodeBlock>
+            <CopyButton onClick={() => handleCopy(roomHelper, 0)}>
+              {isCopied[0].value === true ? 'Copied!' : 'Copy'}
+            </CopyButton>
+            {roomHelper}
+          </CodeBlock>
+          <Spacer />
+          <SubTitleSmall>helpers/setup.js</SubTitleSmall>
+          This will get run at the beginning of the game which will handle all of the fun for us.
+          <CodeBlock>
+            <CopyButton onClick={() => handleCopy(mapGeneration, 1)}>
+              {isCopied[1].value === true ? 'Copied!' : 'Copy'}
+            </CopyButton>
+            {mapGeneration}
+          </CodeBlock>
+          <Spacer />
+          <SubTitle id='combat'>Character</SubTitle>
+          <CopyButton onClick={() => handleCopy(createReactAppCommand, 2)}>
+            {isCopied[2].value === true ? 'Copied!' : 'Copy'}
           </CopyButton>
-          <CodeBlock>{mapGeneration}</CodeBlock>
+          {/* <CodeBlock>{createReactAppCommand}</CodeBlock> */}
+          <Spacer />
+          <SubTitle id='loot'>Loot</SubTitle>
+          <CopyButton onClick={() => handleCopy(createReactAppCommand, 3)}>
+            {isCopied[3].value === true ? 'Copied!' : 'Copy'}
+          </CopyButton>
+          {/* <CodeBlock>{createReactAppCommand}</CodeBlock> */}
+          <Spacer />
+          <SubTitle id='combat'>Combat</SubTitle>
+          <CopyButton onClick={() => handleCopy(createReactAppCommand, 4)}>
+            {isCopied[4].value === true ? 'Copied!' : 'Copy'}
+          </CopyButton>
+          {/* <CodeBlock>{createReactAppCommand}</CodeBlock> */}
+          <Spacer />
+          <SubTitle id='findings'>Findings</SubTitle>
+          Overall I would say this has been a fun experience, but there are definitely some learning curves from a blogging perspective - most noticeably a lack of planning! It was very naive of me to think I could 'code as I go' without really
+          putting too much thought into what this project exactly entails. I knew I had a basic idea of what I wanted to include in this project but I did not have a clear 'mvp', and without a plan on what you're going to deliver
+          you can find yourself in a situation where your project starts to grow arms and legs as well as wasting time re-writing code. That being said, I'm a big fan of 'doing' and a lot of the time I use that as an excuse to go explore and find creative inspiration and I would say
+          that this project may of had a little bit too much of that 😂
+          <Spacer />
+          I wouldn't say that this is a finished project as I do plan to revisit this and expand on what's currently there as well as adding a couple of more features. However it is at a place where I'm comfortable with leaving it for the time being - largely
+          because I want to move onto other things.
           <Spacer />
         </Text>
       </Container>
