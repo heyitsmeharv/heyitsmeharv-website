c6071e79454a81f789985b2d7841a7f7e7ac3676
Adam Harvey <heyitsmeharv@gmail.com>
2024-08-08 17:51:51 +0100

    eks

--- src/components/Posts/AWSContainers.js
+++ src/components/Posts/AWSContainers.js
@@ -21,6 +21,8 @@ import ECSIAMRoles from "../../resources/images/blog/AWSContainers/containers_ec
 import ECSLoadBalancers from "../../resources/images/blog/AWSContainers/containers_ecs_load_balancers.jpeg";
 import ECSEFS from "../../resources/images/blog/AWSContainers/containers_ecs_efs.jpeg";
 import ECSScaling from "../../resources/images/blog/AWSContainers/containers_ecs_scaling.jpeg";
+import ECR from "../../resources/images/blog/AWSContainers/containers_ecr.jpeg";
+import EKS from "../../resources/images/blog/AWSContainers/containers_eks.jpeg";
 
 
 const Wrapper = styled.div`
@@ -230,8 +232,10 @@ const AWSContainers = () => {
           <StyledAnchor href="#ecs"><StyledListItem>Amazon ECS (Elastic Container Service)</StyledListItem></StyledAnchor>
           <StyledAnchor href="#iam-roles-for-ecs"><StyledListItem>IAM Roles for ECS</StyledListItem></StyledAnchor>
           <StyledAnchor href="#load-balancers-integration"><StyledListItem>ECS Load Balancers Integration</StyledListItem></StyledAnchor>
-          <StyledAnchor href="#data-volumes"><StyledListItem>ECS - Data Volumes (EFS)</StyledListItem></StyledAnchor>
+          <StyledAnchor href="#ecs-data-volumes"><StyledListItem>ECS - Data Volumes (EFS)</StyledListItem></StyledAnchor>
           <StyledAnchor href="#auto-scaling"><StyledListItem>ECS Service Auto Scaling</StyledListItem></StyledAnchor>
+          <StyledAnchor href="#ecr"><StyledListItem>Amazon ECR (Elastic Container Registry)</StyledListItem></StyledAnchor>
+          <StyledAnchor href="#eks"><StyledListItem>Amazon EKS (Elastic Kubernetes Service)</StyledListItem></StyledAnchor>
           <Spacer />
           <SubTitle id="s3-introduction">What is Docker?</SubTitle>
           Docker is an open-source platform designed to automate the deployment, scaling, and management of applications within lightweight, portable containers. Containers package an application with all its dependencies
@@ -302,7 +306,7 @@ const AWSContainers = () => {
           <StyledListItem><BoldTextSmall>Application Load Balancer</BoldTextSmall> Supported and works for most use cases.</StyledListItem>
           <StyledListItem><BoldTextSmall>Efficient Data Retrieval</BoldTextSmall> Recommended for high throughput / hight performance use cases, or to pair it with AWS Private Link.</StyledListItem>
           <StyledImage src={ECSLoadBalancers} />
-          <SubTitle id="data-volumes">ECS - Data Volumes (EFS)</SubTitle>
+          <SubTitle id="ecs-data-volumes">ECS - Data Volumes (EFS)</SubTitle>
           What happens if you want to persist data on the container? For this you will need to mount an EFS (Elastic File System) onto the ECS tasks to share data. Tasks running in any AZ will share the same data in the EFS file system.
           <StyledImage src={ECSEFS} />
           <SubTitle id="auto-scaling">ECS Service Auto Scaling</SubTitle>
@@ -315,6 +319,34 @@ const AWSContainers = () => {
           <StyledListItem><BoldText>Step Scaling:</BoldText> Scale based on a specified CloudWatch alarm.</StyledListItem>
           <StyledListItem><BoldText>Scheduled Scaling:</BoldText> Scale based on a specified date / time (predictable changes).</StyledListItem>
           <StyledImage src={ECSScaling} />
+          <SubTitle id="ecr">Amazon ECR (Elastic Container Registry)</SubTitle>
+          Amazon ECR is used to store and manage Docker images. You can choose to store private or public images. On top of being a repository for images, ECR supports image vulnerability scanning, versioning,
+          image tags and image lifecycle.
+          <StyledImage src={ECR} />
+          <SubTitle id="eks">Amazon EKS (Elastic Kubernetes Service)</SubTitle>
+          Amazon Elastic Kubernetes Service (Amazon EKS) is a fully managed service that allows you to run Kubernetes on AWS without needing to install, operate, and maintain your own Kubernetes control plane or nodes.
+          Kubernetes is an open-source system for automating the deployment, scaling, and management of containerized applications, and Amazon EKS simplifies this process by handling much of the operational complexity.
+          <SubTitleSmall>Managed Control Plane</SubTitleSmall>
+          Amazon EKS provides a highly available and secure Kubernetes control plane, which includes the API server, etcd (the key-value store), and other components of the control plane. AWS manages these components, including scaling, patching, and upgrades.
+          <Spacer />
+          <SubTitleSmall>Worker Nodes</SubTitleSmall>
+          <StyledListItem>The worker nodes are the EC2 instances (or Fargate nodes) that run your Kubernetes pods (the smallest deployable units in Kubernetes). While you manage the worker nodes, EKS can help simplify their setup and scaling.</StyledListItem>
+          <StyledListItem>EKS also integrates with AWS Fargate, allowing you to run Kubernetes pods without managing the underlying EC2 instances.</StyledListItem>
+          <Spacer />
+          <SubTitleSmall>Cluster Autoscaling</SubTitleSmall>
+          EKS supports Kubernetes Cluster Autoscaler, which automatically adjusts the number of nodes in your cluster based on the resource requirements of your workloads.
+          <StyledImage src={EKS} />
+          <SubTitleSmall>Node Types</SubTitleSmall>
+          <StyledListItem><BoldText>Managed Node Groups:</BoldText></StyledListItem>
+          <StyledListItemIndent>Creates and manages Nodes (EC2 instances) for you.</StyledListItemIndent>
+          <StyledListItemIndent>Nodes are part of an ASG managed by EKS.</StyledListItemIndent>
+          <StyledListItemIndent>Supports On-Demand or Spot Instances.</StyledListItemIndent>
+          <StyledListItem><BoldText>Self Managed Nodes:</BoldText></StyledListItem>
+          <StyledListItemIndent>Nodes created by you and registered to the EKS cluster and managed by an ASG.</StyledListItemIndent>
+          <StyledListItemIndent>You can use prebuild AMI (EKS Optimized AMI).</StyledListItemIndent>
+          <StyledListItemIndent>Supports On-Demand or Spot Instances.</StyledListItemIndent>
+          <StyledListItem><BoldText>AWS Fargate:</BoldText></StyledListItem>
+          <StyledListItemIndent>No maintenance required; no nodes managed.</StyledListItemIndent>
         </Text>
       </Container>
     </Wrapper>
