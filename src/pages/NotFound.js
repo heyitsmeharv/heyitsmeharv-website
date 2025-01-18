31883556d339cbe87d1c9d52dafa7aaa1f887b1d
Adam Harvey <heyitsmeharv@gmail.com>
2025-01-18 02:11:54 +0000

    put in a 404 for url manipulation

---
+++ src/pages/NotFound.js
@@ -0,0 +1,48 @@
+import ReactGA from 'react-ga';
+import React, { useEffect } from 'react';
+import styled from "styled-components";
+
+// animations
+import SlideInLeft from "../animations/SlideInLeft";
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: calc(100vh - 200px);
+  background-color: #f8f9fa;
+  color: #343a40;
+  animation: ${SlideInLeft} 0.5s forwards;
+`;
+
+const Message = styled.h1`
+  font-size: 5rem;
+  margin-bottom: 1rem;
+`;
+
+const Description = styled.p`
+  font-size: 2rem;
+  margin-bottom: 2rem;
+  text-align: center;
+  max-width: 600px;
+`;
+
+const NotFoundPage = () => {
+
+  // analytics
+  useEffect(() => {
+    if (window.location.hostname !== "localhost") {
+      ReactGA.pageview('/notfound');
+    }
+  }, []);
+
+  return (
+    <Container>
+      <Message>404</Message>
+      <Description>Oops! The page you're looking for doesn't exist.</Description>
+    </Container>
+  );
+};
+
+export default NotFoundPage;
